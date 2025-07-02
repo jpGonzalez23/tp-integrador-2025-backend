0e5d9d288c04089c1d9eee5d94fd76df5730ad52
Juan Pablo Gonzalez <juanpigonzalez72@gmail.com>
2025-07-02 17:59:26 -0300

    Se agrega la carpeta utils, ejs para crear views

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ import cors from "cors";
 import enviroments from "./src/api/config/enviroments.js";
 import { loginRoutes, productosRoutes } from "./src/api/routes/index.js";
 import { loggerUrl } from "./src/api/middlewares/middlewares.js";
+import { join } from "./src/api/utils/index.js";
 
 const app = express();
 const PORT = enviroments.port;
+
+app.set("view engine", "ejs");
+
+app.set("views", join("src/views"));
+
+app.use(express.static(join, "src/public"));
 
 /*** 
  *  Middlewares
